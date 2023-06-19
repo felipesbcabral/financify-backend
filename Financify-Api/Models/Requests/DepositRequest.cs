07ab67f1306aafea9cd5197c22e271589e3de864
FelipeCabralz <felipesbcabral@gmail.com>
2023-06-19 15:09:54 -0300

    :art: Adds deposit endpoint

---
+++ Financify-Api/Models/Requests/DepositRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Financify_Api.Models.Requests
+{
+    public class DepositRequest
+    {
+        public decimal Valor { get; set; }
+        public string MetodoPagamento { get; set; }
+        public string NumeroCartao { get; set; }
+        public string DataValidade { get; set; }
+        public string CodigoSeguranca { get; set; }
+        public string Senha { get; set; }
+    }
+}
